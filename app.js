5de6af1ed6e6a3ac54bf89a064f7d16eb6b00f17
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-28 17:16:25 -0500

    tryna make it work

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
+
+app.set('port', (process.env.PORT || 5000));
+
 app.get("/", function (req,res) {
     res.sendFile(__dirname + 'index.html');
 });
 
-app.listen(3000);
-
-console.log("express running at http://localhost:3000");
+app.listen(app.get('port'), function() {
+  console.log('running on port', app.get('port'));
+});
